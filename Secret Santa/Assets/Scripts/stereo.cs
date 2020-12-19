da3d756805e418719d3b1449ce86977359f04ad4
bernhard100 <65234210+bernhard100@users.noreply.github.com>
2020-12-19 22:39:58 +0100

    stereo enemy prototype added

---
+++ Secret Santa/Assets/Scripts/stereo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stereo : MonoBehaviour
+{
+    [SerializeField] float shootInterval = 1f;
+    [SerializeField] Transform target;
+    [SerializeField] GameObject projectile;
+    public bool shooting{
+        set{
+            if(value){
+                InvokeRepeating("LaunchWave", 0f, shootInterval);
+            }else{
+                CancelInvoke("LaunchWave");
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        shooting = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LaunchWave(){
+        Instantiate(projectile, transform.position, Quaternion.LookRotation(target.position - transform.position, Vector3.up));
+    }
+}
